417f3020df7b7dfd3432de83a2d76093a718bd7f
MFMFloppY <tkomarov098@gmail.com>
2025-02-18 23:21:31 +0300

    Я сделал время

---
+++ hello.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from keyboard import is_pressed
+
+
+def current_date():
+    """Выводит текущее время."""
+    
+    while True:
+
+        if is_pressed("Enter"):
+            print(str(datetime.now().time())[:-7])
+            
+            break
+
+
+if __name__ == "__main__":
+    current_date()
+
+
+
+
+
+
+
